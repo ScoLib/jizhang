ccd7a95c0a45fc531dea6a44d2c617d4da60c07b
klgd <slice1213@gmail.com>
2018-11-09 18:27:26 +0800

    update

--- wepy.config.js
+++ wepy.config.js
@@ -6,11 +6,6 @@ module.exports = {
   eslint: false,
   cliLogs: !prod,
   build: {
-    web: {
-      htmlTemplate: path.join('src', 'index.template.html'),
-      htmlOutput: path.join('web', 'index.html'),
-      jsOutput: path.join('web', 'index.js')
-    }
   },
   resolve: {
     alias: {
